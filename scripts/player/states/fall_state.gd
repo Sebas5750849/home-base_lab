0d7b96c15127d97a4f9011b614888b61bef3e241
anoukstassen <a.stassen@student.tudelft.nl>
2025-12-06 12:04:01 +0100

    fixed a bug where the player gets stuck in a ceiling when going from crouch to fall

--- scripts/player/states/fall_state.gd
+++ scripts/player/states/fall_state.gd
@@ -4,7 +4,7 @@ extends PlayerState
 
 func enter_state():
 	Name = "Falling"
-	
+		
 func exit_state():
 	pass
 	
@@ -13,6 +13,9 @@ func draw():
 	
 func update_state(delta):
 	Player.handle_gravity(delta, Player.GRAVITY_FALL)
+	if above_head_is_clear():
+		Player.collision_shape.shape = Player.standing_shape
+		Player.collision_shape.position.y = -22
 	Player.horizontal_movement(Player.AIR_ACCELERATION, Player.AIR_DECELERATION)
 	handle_jump_buffer()
 	Player.handle_landing()
@@ -28,3 +31,6 @@ func handle_animations():
 func handle_jump_buffer():
 	if Player.key_jump:
 		Player.jump_buffer_timer.start(Player.JUMP_BUFFER_TIME)
+
+func above_head_is_clear() -> bool:
+	return not Player.crouch_ray_1.is_colliding() and not Player.crouch_ray_2.is_colliding()
